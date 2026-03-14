3eabbd91e7831f1e109c69d0de275e9328649d7b
Antony <markpakoeningrat@gmail.com>
2026-03-14 11:36:03 -0600

    Update youtube_m3ugrabber.py

--- youtube_m3ugrabber.py
+++ youtube_m3ugrabber.py
@@ -1,69 +1,27 @@
-#! /usr/bin/python3
+import subprocess
 
-banner = r'''
-#########################################################################
-#      ____            _           _   __  __                           #
-#     |  _ \ _ __ ___ (_) ___  ___| |_|  \/  | ___   ___  ___  ___      #
-#     | |_) | '__/ _ \| |/ _ \/ __| __| |\/| |/ _ \ / _ \/ __|/ _ \     #
-#     |  __/| | | (_) | |  __/ (__| |_| |  | | (_) | (_) \__ \  __/     #
-#     |_|   |_|  \___// |\___|\___|\__|_|  |_|\___/ \___/|___/\___|     #
-#                   |__/                                                #
-#                                  >> https://github.com/benmoose39     #
-#########################################################################
-'''
+channels = [
+    ("NASA Live","https://www.youtube.com/@NASA/live","https://upload.wikimedia.org/wikipedia/commons/e/e5/NASA_logo.svg","Ciencia"),
+    ("Canal Once","https://www.youtube.com/@CanalOnceIPN/live","https://upload.wikimedia.org/wikipedia/commons/2/2b/Canal_Once_logo_2025.png","Noticias"),
+    ("NMás","https://www.youtube.com/@nmas/live","https://upload.wikimedia.org/wikipedia/commons/f/f6/Logo-nmas.png","Noticias"),
+    ("Kings League Mexico","https://www.youtube.com/@kingsleague_mex/live","https://upload.wikimedia.org/wikipedia/commons/9/92/Kings-league.png","Entretenimiento"),
+    ("Kings League España","https://www.youtube.com/@KingsLeagueOfficial/live","https://upload.wikimedia.org/wikipedia/commons/9/92/Kings-league.png","Entretenimiento")
+]
 
-import requests
-import os
-import sys
+playlist = '#EXTM3U\n'
 
-windows = False
-if 'win' in sys.platform:
-    windows = True
+for name,url,logo,group in channels:
+    try:
+        stream = subprocess.check_output(
+            ["yt-dlp","-g",url],
+            stderr=subprocess.DEVNULL
+        ).decode().strip()
 
-def grab(url):
-    response = requests.get(url, timeout=15).text
-    if '.m3u8' not in response:
-        #response = requests.get(url).text
-        if '.m3u8' not in response:
-            if windows:
-                print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
-                return
-            #os.system(f'wget {url} -O temp.txt')
-            os.system(f'curl "{url}" > temp.txt')
-            response = ''.join(open('temp.txt').readlines())
-            if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
-                return
-    end = response.find('.m3u8') + 5
-    tuner = 100
-    while True:
-        if 'https://' in response[end-tuner : end]:
-            link = response[end-tuner : end]
-            start = link.find('https://')
-            end = link.find('.m3u8') + 5
-            break
-        else:
-            tuner += 5
-    print(f"{link[start : end]}")
+        playlist += f'#EXTINF:-1 tvg-logo="{logo}" group-title="{group}",{name}\n'
+        playlist += stream + "\n"
 
-print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"')
-print(banner)
-#s = requests.Session()
-with open('youtube_channel_info.txt') as f:
-    for line in f:
-        line = line.strip()
-        if not line or line.startswith('~~'):
-            continue
-        if not line.startswith('https:'):
-            line = line.split('|')
-            ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
-            tvg_logo = line[2].strip()
-            tvg_id = line[3].strip()
-            print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
-        else:
-            grab(line)
-            
-if 'temp.txt' in os.listdir():
-    #os.system('rm temp.txt')
-    os.system('rm watch*')
+    except:
+        pass
+
+with open("youtube.m3u","w") as f:
+    f.write(playlist)
